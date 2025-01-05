33c3bfc539e14005f399375f038ec95c5cd282bb
devisitem <tnine923@gmail.com>
2025-01-05 14:29:42 +0900

    PassingCars 추가

---
+++ src/main/java/prefixsums/passingcars/PassingCars.java
@@ -0,0 +1,27 @@
+package prefixsums.passingcars;
+
+public class PassingCars {
+
+    /**
+     * @see <a href="https://app.codility.com/programmers/lessons/5-prefix_sums/passing_cars/">Passing Cars</a>
+     * <p>N개의 정수를 가지며 비어있지 않은 배열 A가 주어진다. A 배열의 연속적인요소는 길위의 연속적인 차들을 나타낸다.</p>
+     * <p>A 배열은 0초 또는 1초만 포함 한다.</p>
+     * <ul>
+     *     <li>0은 동쪽을 여행하는 차를 나타낸다.</li>
+     *     <li>1은 서쪽을 여행하는 차를 나타낸다.</li>
+     * </ul>
+     * <p>목표는 통과하는 차들의 개수를 세는것이다. 차들(P, Q)은 한쌍을 말하며, 0 <= P < Q < N 인 경우, P가 동쪽으로 여행할 때고, Q는 서쪽으로 여행할 때 이다. </p>
+     * <p>예를 들어 A 배열이 아래와 같다.</p>
+     * ```
+     * A[0] = 0
+     * A[1] = 1
+     * A[2] = 0
+     * A[3] = 1
+     * A[4] = 1
+     * ```
+     * <p>5개의 통과하는 차들의 쌍이다: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).</p>
+     */
+    public static int solution(int [] A) {
+        return 0;
+    }
+}
